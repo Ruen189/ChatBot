4db968b2e3dc02900c2f1c6359ccce46d10428be
RostislavGordeev <144150642+Ruen189@users.noreply.github.com>
2025-07-05 14:37:32 +0500

    Delete support.py

--- support.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-# repo_id может отличаться — убедитесь на HF, что именно называется Llama-3.2-1B
-local_folder = snapshot_download(
-    repo_id="meta-llama/Llama-3.2-1B",
-    use_auth_token=True
-)
-print("Модель скачана в:", local_folder)
