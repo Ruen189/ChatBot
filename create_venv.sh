f2dbe5a4c23c35e489cfa9f46b61116aafbab4ad
Ilya Korobitsyn <159927654+Wobir@users.noreply.github.com>
2025-08-07 21:30:20 +0300

    Update create_venv.sh

--- create_venv.sh
+++ create_venv.sh
@@ -2,7 +2,7 @@
 sudo apt update && sudo apt upgrade
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.10
+sudo apt install python3.10-full
 python3.10 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
